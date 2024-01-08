c79c9436a1418b030752aaa9e8868061b4f68cdb
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2024-01-08 13:57:28 -0300

    Exercicio ES6

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-// Classe de abstracao
-function Veiculo(nome, velocidade, uso) {
-  this.nome = nome;
-  this.velocidade = velocidade;
-  this.uso = uso;
+class Alunos {
+  constructor(nome, nota) {
+    this.nome = nome;
+    this.nota = nota;
+  }
 }
 
-// Classes herdeiras
-function Carro(marca) {
-  this.marca = marca;
-  Veiculo.call(this, "Carro", "média", "terrestre");
-}
+const joao = new Alunos("Joao", 7);
+const maria = new Alunos("Maria", 8);
+const pedro = new Alunos("Pedro", 9);
+const julio = new Alunos("Pulio", 6);
+const armando = new Alunos("Armando", 4);
+const cesar = new Alunos("Cesar", 0);
 
-function Aviao(companhiaAerea) {
-  this.companhiaAerea = companhiaAerea;
-  Veiculo.call(this, "Avião", "alta", "aereo");
-}
+const todosOsAlunos = [];
+todosOsAlunos.push(joao, maria, pedro, julio, armando, cesar);
 
-// Instâncias
-const veiculo1 = new Aviao("azul");
-const veiculo2 = new Carro("toyota");
-const veiculo3 = new Carro("chevrolet");
+const alunosAprovados = todosOsAlunos.filter(function (item) {
+  return item.nota >= 6;
+});
 
-console.log(veiculo1);
-console.log(veiculo2);
-console.log(veiculo3);
+console.log(alunosAprovados);
